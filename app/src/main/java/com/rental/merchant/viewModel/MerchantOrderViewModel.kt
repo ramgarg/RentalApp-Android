fc7a60ae710eb7bb5e3ff8e64bc6391930afda71
himanshu316 <62279973+himanshu316@users.noreply.github.com>
2020-03-20 10:44:10 +0530

    updated

--- app/src/main/java/com/rental/merchant/viewModel/MerchantOrderViewModel.kt
+++ app/src/main/java/com/rental/merchant/viewModel/MerchantOrderViewModel.kt
@@ -3,7 +3,6 @@ package com.rental.merchant.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.rental.customer.dashboard.model.modelclass.HomeResponse
-import com.rental.customer.dashboard.model.repositry.OrderRepository
 import com.rental.merchant.model.repository.MerchantOrderRepository
 
 class MerchantOrderViewModel : ViewModel() {
