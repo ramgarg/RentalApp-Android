bdf9b238a33b959e3fd7377f370fce2655d7e49f
himanshu316 <62279973+himanshu316@users.noreply.github.com>
2020-04-09 21:20:09 +0530

    updated UI

--- app/src/main/java/com/rental/merchant/view/activity/MerchantAddVehicle.kt
+++ app/src/main/java/com/rental/merchant/view/activity/MerchantAddVehicle.kt
@@ -1,6 +1,11 @@
 package com.rental.merchant.view.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.rental.R
 import com.rental.customer.dashboard.model.modelclass.Data
@@ -25,6 +30,10 @@ class MerchantAddVehicle : AppCompatActivity(), RecyclerViewItemClick {
             this, img_back, img_menu, img_notification,
             toolbar_title, getString(R.string.vehicle))
 
+        typeSpinnerData()
+        categorySpinnerData()
+        subCategorySpinnerData()
+
        /* merchantViewModelCustomer= ViewModelProviders.of(this).get(CustomerHomeViewModel::class.java)
         merchantViewModelCustomer.getHomeResponse().observe(this, Observer {
             rec_add_veichle.adapter= CategoryAdapter(it.data,this,this)
@@ -32,6 +41,68 @@ class MerchantAddVehicle : AppCompatActivity(), RecyclerViewItemClick {
 
             //search(arrayList)
         })*/
+
+    }
+
+    private fun subCategorySpinnerData() {
+        val select_subcategory = resources.getStringArray(R.array.SelectSubCategory)
+
+        val spinner = findViewById<Spinner>(R.id.sp_select_subcategory)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, select_subcategory)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    Toast.makeText(this@MerchantAddVehicle, getString(R.string.selected_item) + " " + "" + select_subcategory[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+    }
+
+    private fun categorySpinnerData() {
+        val select_category = resources.getStringArray(R.array.SelectCategory)
+
+        val spinner = findViewById<Spinner>(R.id.sp_select_category)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, select_category)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    Toast.makeText(this@MerchantAddVehicle, getString(R.string.selected_item) + " " + "" + select_category[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+    }
+
+    private fun typeSpinnerData() {
+        val select_type = resources.getStringArray(R.array.SelectType)
+
+        // access the spinner
+        val spinner = findViewById<Spinner>(R.id.sp_select_type)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, select_type)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    Toast.makeText(this@MerchantAddVehicle, getString(R.string.selected_item) + " " + "" + select_type[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
     }
 
     override fun onItemClick(item: Data) {
