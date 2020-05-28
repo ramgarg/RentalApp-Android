7685717d1786a9c12b0f0dc14dfaf4f70d56ca4d
himanshu316 <62279973+himanshu316@users.noreply.github.com>
2020-05-28 20:37:34 +0530

    Bug Fixes

--- app/src/main/java/com/eazyrento/customer/dashboard/view/fragment/ProfieFragment.kt
+++ app/src/main/java/com/eazyrento/customer/dashboard/view/fragment/ProfieFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.eazyrento.R
+import com.eazyrento.appbiz.AppBizLogger
+import com.eazyrento.common.view.fragment.BaseFragment
+import com.eazyrento.login.model.modelclass.ProfileModelReqRes
+import com.eazyrento.login.view.ProfileData
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_profile.*
 
-open class ProfieFragment : Fragment() {
+open class ProfieFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -16,6 +21,33 @@ open class ProfieFragment : Fragment() {
         return view
 
 }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        ProfileData().setData(requireActivity())
+
+        /* callAPI()?.let {
+             it.observeApiResult(
+                 it.callAPIFragment<ProfileUserViewModel>(this).getProfileUser()
+                 , viewLifecycleOwner, requireActivity()
+             )
+         }*/
+
+    }
+    override fun <T> onSuccessApiResult(data: T) {
+        AppBizLogger.log(AppBizLogger.LoggingType.DEBUG,data.toString())
+
+        val userProfile = data as ProfileModelReqRes
+
+        tv_customer_name.text=userProfile.user_profile.full_name
+        customer_email.text=userProfile.user_profile.email
+        customer_phone.text=userProfile.user_profile.mobile_number
+        customer_address.text=userProfile.user_profile.address_info.address_line+" "+userProfile.user_profile.address_info.state+" "+userProfile.user_profile.address_info.country
+
+        Picasso.with(requireContext()).load(userProfile.user_profile.profile_image).into(img_profile)
+
+
+    }
 
 
 }
